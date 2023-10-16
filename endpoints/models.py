ccf5c42da7c30cb0c5a76b22b68d81bf31c78a2c
Ryan Sullivan <rsull@wharton.upenn.edu>
2023-10-16 10:10:33 -0400

    Some models

--- endpoints/models.py
+++ endpoints/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.product.name
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.name
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.name
+   
