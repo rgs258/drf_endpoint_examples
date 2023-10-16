664e7f49b188f1fa86af55264a2feef06b8ae34b
Ryan Sullivan <rsull@wharton.upenn.edu>
2023-10-16 10:26:22 -0400

    Fix models.py

--- endpoints/admin.py
+++ endpoints/admin.py
@@ -2,9 +2,9 @@
 
 from django.contrib import admin
 
-from .models import User, Product, Order, Address, Review
+from .models import Customer, Product, Order, Address, Review
 
-admin.site.register(User)
+admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Address)
--- endpoints/models.py
+++ endpoints/models.py
@@ -4,12 +4,16 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
 
     def __str__(self):
+        """
+
+        :return: The name of the customer
+        """
         return self.name
 
 
@@ -19,31 +23,49 @@ class Product(models.Model):
     description = models.CharField(max_length=100)
 
     def __str__(self):
+        """
+
+        :return: The name of the product
+        """
         return self.name
 
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
     def __str__(self):
-        return self.product.name
+        """
+
+        :return: The name of the customer and the product they ordered
+        """
+
+        return self.customer.name + " " + self.product.name
 
 
 class Address(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.user.name
+        """
+
+        :return: The name of the customer and their address
+        """
+
+        return self.customer.name + " " + self.address
 
 
 class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     rating = models.IntegerField()
     review = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.user.name
+        """
+        :return: The name of the customer and the product they reviewed
+        """
+
+        return self.customer.name + " " + self.product.name
