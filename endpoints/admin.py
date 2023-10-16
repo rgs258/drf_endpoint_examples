47ecff63f4cc7fbb8e7f3e093464a44b1adf450d
Ryan Sullivan <rsull@wharton.upenn.edu>
2023-10-16 10:18:27 -0400

    Admin

--- endpoints/admin.py
+++ endpoints/admin.py
@@ -1,1 +1,11 @@
 # Register your models here.
+
+from django.contrib import admin
+
+from .models import User, Product, Order, Address, Review
+
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Address)
+admin.site.register(Review)
